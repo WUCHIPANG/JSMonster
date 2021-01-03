b5728dacd73451dcae0cb50e5184bc6dc8901dd0
olga.wu <chipang59@gmail.com>
2021-01-03 19:55:30 +0800

    add

---
+++ js/js.js
@@ -0,0 +1,15 @@
+var percent = 0
+
+function eatCount() {
+  $('.monsterText').html('We are<br>SQUARE<br>MONSTER!')
+}
+
+var timer = setInterval(function () {
+  $('.bar').css('width', percent + '%')
+  percent += 1
+  if (percent > 100) {
+    $('.pageLoading').addClass('complete')
+    clearInterval(timer)
+    setTimeout(eatCount, 3000)
+  }
+}, 30)
